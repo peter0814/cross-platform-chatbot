dab001fc1a754375e43e21458ef5f7b5360e1e1a
peter0814 <74885247+peter0814@users.noreply.github.com>
2020-11-25 20:03:52 +0800

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,54 @@
+const KKBOXMessage = require('./message/KKBOXMessage');
+const kkbox = global.kkbox;
+const kkassistant = global.kkassistant
+
+const welcomeMessage = 'Hi~ 本 Bot 是用 https://rebrand.ly/ic-chatbot-github 開源程式碼打造\n\n' +
+    '您可以問我\n' +
+    '音樂：「播放怪美的」；「播放自傳專輯的歌」；「我要聽鄉村音樂」；「我要聽日文新歌」\n' + 
+    '音樂活動：「查詢高雄的活動」；「查詢吳卓源的演場會」；「查詢兩廳院的表演」\n' +
+    '影音內容：「查詢影片排球少年」；「查詢日劇半澤直樹」\n';
+
+
+exports.HandleLineMessage = async context => {
+    if (context.event.isText) {
+        kkassistant.nlu(context.event.text, context.session.id)
+            .then(nluResp => {
+                if (nluResp.directives.length > 0) {
+                    if(nluResp.directives[0].type == 'AudioPlayer.Play') {
+                        return kkbox.fetchTracks(nluResp.directives[0].playlist.data);
+                    } else { // Event.Metadata & Video.Metadata
+                        return nluResp.directives[0];
+                    }
+                }
+                else {
+                    console.error('Error: ', nluResp);
+                    context.sendText(nluResp.outputSpeech.text);
+                    throw new Error('KKBOX Assistant NLP Error');
+                }
+            })
+            .then(items => new KKBOXMessage(items).toLineMessage())
+            .then(({ altText, template }) => context.sendImageCarouselTemplate(altText, template))
+            .catch(error => {
+                console.error('Error: ', error);
+            });
+    }
+}
+
+exports.HandleFollow = async context => {
+    await context.sendText(welcomeMessage);
+}
+
+exports.HandleMessengerMessage = async context => {
+    if (context.event.isText) {
+        await context.sendText(`received the text message: ${context.event.text}`);
+    }
+curl -s \
+ -H 'Content-Type: application/json' \
+ -H 'Authorization: Bearer '${TOKEN} \
+ --url 'https://api.kkbox.com/v1.1/albums/Wku_flBDIvVnmt5aQM/tracks' \
+ | jq '.data[] | .id' \
+ | xargs -n 1 -I % curl -s -H 'Content-Type: application/json' \
+ -H 'Authorization: Bearer '${TOKEN} \
+ --url 'https://api.kkbox.com/v1.1/me/favorite' \
+ -d '{"track_id": "%"}'
+}
